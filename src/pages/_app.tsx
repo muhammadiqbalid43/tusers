45ff46745f38d9ae66c39c50facab7074605895a
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-19 07:17:30 +0700

    feat: tanstack setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import AppSidebar from "@/components/sidebar/app-sidebar";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+
+      <main className="ring-1 overflow-x-hidden">
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </main>
+    </SidebarProvider>
+  );
 }
