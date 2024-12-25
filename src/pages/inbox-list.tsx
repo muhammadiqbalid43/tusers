9bc3d54a2bc4098dfbfa613faa3764b94451aefa
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-25 09:34:00 +0700

    feat: add inbox page

---
+++ src/pages/inbox-list.tsx
@@ -0,0 +1,11 @@
+import List from "@/components/inbox/list";
+
+export default function InboxList() {
+  return (
+    <div className="p-8  w-full ">
+      <div className="container mx-auto">
+        <List />
+      </div>
+    </div>
+  );
+}
