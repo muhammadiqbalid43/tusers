ddb9832aba127811e4cf415d9d7d7c2567b158d4
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-25 09:05:58 +0700

    fix: images config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dummyjson.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        port: "",
+        pathname: "/products/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
